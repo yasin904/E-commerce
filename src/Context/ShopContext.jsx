fffab956f0c7c009ee68727399c53f2f7349e3d1
Mohammed Yasin <mohammed.yasin@olacabs.com>
2024-02-22 12:56:46 +0530

    changes made

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { createContext } from "react";
-import all_product from "../Components/Assets/all_product";
+// import React from "react";
+// import { createContext } from "react";
+// import all_product from "../Components/Assets/all_product";
 
-export const shopContext = createContext(null);
+// export const shopContext = createContext(null);
 
-const ShopContextProvider = (props) =>{
-    const contextValue = { all_product};
+// const ShopContextProvider = (props) =>{
+//     const contextValue = { all_product};
 
-    return(
-        <ShopContextProvider value = {contextValue}>
-            {props.children}
-        </ShopContextProvider>
-    )
+//     return(
+//         <ShopContextProvider value = {contextValue}>
+//             {props.children}
+//         </ShopContextProvider>
+//     )
 
-}
+// }
 
-export default ShopContextProvider
+// export default ShopContextProvider
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,10 +1,10 @@
-import React,{ useContext } from "react";
-import  ShopContext  from "../Context/ShopContext";
+// import React,{ useContext } from "react";
+// import  ShopContext  from "../Context/ShopContext";
 import './CSS/ShopCategory.css'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 
 const ShopCategory = (props) =>{
-    const {all_product} = useContext(ShopContext);
+    // const {all_product} = useContext(ShopContext);
     return(
         <div className="shop-category">
             <img src={props.banner} alt=""/>
